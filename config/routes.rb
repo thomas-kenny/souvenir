0408d2b6d11ea3b4f54bd4515c45955c5ae7380b
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-08-18 15:16:49 +0100

    routes amended

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get '/jobs', to: 'pages#jobs'
+  constraints(host: 'https://souvenirscenicstudios.herokuapp.com/') do
+    get '/:param' => redirect("https://www.souvenir.co.uk/#{param}")
+end
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
