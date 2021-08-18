fe52afc868dfbef1aee00a7dd9902827985e60ed
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-08-18 15:48:12 +0100

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'www.souvenir.co.uk'
   get '/fashion', to: 'pages#fashion'
   get '/theatre', to: 'pages#theatre'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get '/jobs', to: 'pages#jobs'
-  constraints(:host => "https://souvenirscenicstudios.herokuapp.com") do
-    root :to => redirect("https://www.souvenir.co.uk")
-    match '/*path', :to => redirect {|params| "http://www.souvenir.co.uk/#{params[:path]}"}
-  end
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
